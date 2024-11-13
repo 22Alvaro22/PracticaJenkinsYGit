5105e88b9fe5c50fe4dc1c7a4de5422fd895b8d0
22Alvaro22 <166013801+22Alvaro22@users.noreply.github.com>
2024-11-13 20:52:42 +0100

    Subo builts.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+mkdir web
+index.sh>index.html
+cp index.html web
+pagina1.sh>pagina1.html
+cp pagina1.html web
+pagina2.sh>pagina2.html
+cp pagina2.html web
+pagina3.sh>pagina3.html
+cp pagina3.html web
